205e47e50f667d8f6bc7d00cba7ef2c5e85d8766
Marcos Ireneo Barbas <barbasmarcosi@gmail.com>
2022-08-10 08:57:31 -0300

    feat(Checkout): base structure

--- src/containers/Checkout.jsx
+++ src/containers/Checkout.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
 import '../styles/components/Checkout.css';
 
-const Checkout = () => <h1 className="Checkout">Checkout</h1>;
+const Checkout = () => (
+  <div className="Checkout">
+    <div className="Checkout-content">
+      <h3>Order list:</h3>
+      <div className="Checkout-item">
+        <div className="Checkout-element">
+          <h4>Item name</h4>
+          <span>$10</span>
+        </div>
+        <button type="button">Delete</button>
+      </div>
+    </div>
+    <div className="Checkout-sidebar">
+      <h3>Total Price: $10</h3>
+      <button type="button">Continue order</button>
+    </div>
+  </div>
+);
 
 export default Checkout;
