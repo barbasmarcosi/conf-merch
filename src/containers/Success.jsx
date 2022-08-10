a1bf2485de02fe04fe83645855d1661d0d66a29f
Marcos Ireneo Barbas <barbasmarcosi@gmail.com>
2022-08-10 09:17:03 -0300

    feat(Success): basic structure

--- src/containers/Success.jsx
+++ src/containers/Success.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import '../styles/components/Success.css';
 
-const Success = () => <h1 className="Success">Success</h1>;
+const Success = () => <div className="Success">
+    <div className="Success-content">
+        <h2>Client, Thanks for your order</h2>
+        <span>Your order will arrive in the next 3 days to your address:</span>
+        <div className="Success-map">
+            Google Maps
+        </div>
+    </div>
+</div>;
 
 export default Success;
