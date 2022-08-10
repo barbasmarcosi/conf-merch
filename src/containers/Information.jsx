f06930c9331861307979619091a30f982bf449c7
Marcos Ireneo Barbas <barbasmarcosi@gmail.com>
2022-08-10 09:10:00 -0300

    feat(Information): basic structure

--- src/containers/Information.jsx
+++ src/containers/Information.jsx
@@ -1,5 +1,43 @@
 import React from 'react';
 import '../styles/components/Information.css';
 
-const Information = () => <h1 className="Information">Information</h1>;
+const Information = () => (
+  <div className="Information">
+    <div className="Information-content">
+      <div className="Information-head">
+        <h2>Contact Information:</h2>
+      </div>
+      <div className="Information-form">
+        <form action="">
+        <input type="text" placeholder='Full name' name="name" />
+        <input type="text" placeholder='E-Mail' name="email" />
+        <input type="text" placeholder='Address' name="address" />
+        <input type="text" placeholder='Apartment' name="apartment" />
+        <input type="text" placeholder='City' name="city" />
+        <input type="text" placeholder='County' name="country" />
+        <input type="text" placeholder='State' name="state" />
+        <input type="text" placeholder='Postal Code' name="postalCode" />
+        <input type="text" placeholder='Phone' name="phone" />
+        </form>
+      </div>
+      <div className="Information-buttons">
+        <div className="Information-back">
+            Back
+        </div>
+        <div className="Information-next">
+            Pay
+        </div>
+      </div>
+    </div>
+    <div className="Information-sidebar">
+        <h3>Order:</h3>
+        <div className="Information-item">
+            <div className="Information-element">
+                <h4>Item name: </h4>
+                <span>Price: $10</span>
+            </div>
+        </div>
+    </div>
+  </div>
+);
 export default Information;
